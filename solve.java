51ae02a895c0866c3903aae7aba5fc412381b7bb
justin__ <31811490+coffeeandc0d3@users.noreply.github.com>
2022-01-29 17:55:10 -0600

    added hdmi-toggle

--- solve.java
+++ solve.java
@@ -14,24 +14,28 @@ import java.io.*;
 	xrandr | grep -w connected  | awk -F'[ ]' '{print $1}'
 	
 Pass it as a parameter when you run 'java solve.java' `<Arguments-Here>`
-This gives the progaram the  ID of your connected display.
+This gives the progaram the ID of your connected display.
 *Note* the above arguments are enclosed with back-ticks(`) - not the apostrophe. 
-Alternatively there's radio buttons for all display ID types
+
+Alternatively there's radio buttons for all display ID types.
 This includes types not on your system. 
 Overall it's meant to provide a reliable trial & error approach... 
-...at the cost of speed, optimization, and everything good. 
+...at the cost of speed, optimization, & everything good. 
+
+<todo> allow for entering in your display type, JTextField
+
 */
 
 public class solve extends JFrame implements ChangeListener 
 {
-	static JRadioButton[] buttonDisplays;
 	static String ConnectedDisplay, SystemBrightness;
+	static JRadioButton[] buttonDisplays;
 	static JFrame frame;
 
 //	Slider to adjust brightness
 	static JSlider sliderButton;
 
-//	Label to show text of current % set 
+//	Label to show % set to 
 	static JLabel label;
 
 	// main class
@@ -45,11 +49,11 @@ public class solve extends JFrame implements ChangeListener
 			float currentBrightness = Float.parseFloat(SystemBrightness);	
 			System.out.println("\nCurrent brightness: " + currentBrightness);
 	
-			// creat e a new frame
+//			Standard jframe being initialized 
 			frame = new JFrame("Set Current Brightness");
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	
-			// create a object 
+			// Serves as the reference  
 			solve s = new solve();
 			// create label  
 			label = new JLabel();
@@ -61,12 +65,13 @@ public class solve extends JFrame implements ChangeListener
 			ButtonGroup group = new ButtonGroup();
 	 
 			int count = 1;
-			buttonDisplays = new JRadioButton[5];
+//			By default supports: DP1-4, LVDS-1, and HDMI-1
+			buttonDisplays = new JRadioButton[6];
 			for (int i = 0; i < 4; i++)
 			{
 				buttonDisplays[i] = new JRadioButton();
 
-//				This removes need for array of DP:(1-4)
+//				This removes need for string[] of DP:(1-4)
 				buttonDisplays[i].setText("DP-" + count); 
 				buttonDisplays[i].setBounds(i+10,i+20,20,20);
 				buttonDisplays[i].setVisible(true);
@@ -76,7 +81,7 @@ public class solve extends JFrame implements ChangeListener
 				++count;
 			}	
 
-// 			LVDS Support:
+// 			LVDS Support: 
 			buttonDisplays[4] = new JRadioButton();
 			buttonDisplays[4].setText("LVDS-1");
 			buttonDisplays[4].setBounds(30, 200,10,10);
@@ -85,8 +90,17 @@ public class solve extends JFrame implements ChangeListener
 			p.add(buttonDisplays[4]);
 			group.add(buttonDisplays[4]);
 
+//			HDMI-1 Support: 
+			buttonDisplays[5] = new JRadioButton();
+			buttonDisplays[5].setText("HDMI-1");
+			buttonDisplays[5].setBounds(30, 200,10,10);
+			buttonDisplays[5].setVisible(true);
+
+			p.add(buttonDisplays[5]);
+			group.add(buttonDisplays[5]);
+
 //			Last param ensures slider starts at current system brightness
-			sliderButton = new JSlider(0, 100, (int)(currentBrightness * 100));
+			sliderButton = new JSlider(0, 100, (int)(currentBrightness * 100	));
 			
 			// paint the ticks and tracks
 			sliderButton.setPaintTrack(true);
@@ -128,9 +142,9 @@ public class solve extends JFrame implements ChangeListener
 			}
 	}
 
-	// Each time slider is changed, will execute a shell command 
-	// to adjust brightness with last parameter being the desired
-	// brightness. This parameter will be equal to value of the slider. 
+// Each time slider is changed, will execute a shell command 
+// to adjust brightness with last parameter being the desired
+// brightness. This parameter will be equal to value of the slider. 
 	public void stateChanged(ChangeEvent e)
 	{
 		float currentLevel = ((float)sliderButton.getValue() / 100);
@@ -147,14 +161,14 @@ public class solve extends JFrame implements ChangeListener
            }
     }
 
-//  Retrieves display type user selected
+//  Returns current display type user selected
     public static JRadioButton getChosenDisplay(JRadioButton[] buttonsOther) 
 	{
 		for (JRadioButton b : buttonsOther)
 		{
 			if (b.isSelected())
 			{
-				System.out.println("Selected: " + b.getText());
+//				System.out.println("Selected: " + b.getText());
 				return b;		
 			}
 		}
