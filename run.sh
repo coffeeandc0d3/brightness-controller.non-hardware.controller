682a58e956bcb3d7895ee1e3dfea363090da16b9
justin__ <31811490+coffeeandc0d3@users.noreply.github.com>
2022-04-06 15:00:05 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-java solve `xrandr --verbose | awk '/ connected /{}/Brightness:/{print $2}'` `xrandr | grep -w connected | awk -F'[ ]' '{print $1}'`
+javac solve.java
+
+path=`echo /home/$USER`
+dir=$path/build/brightness-controller.non-hardware.controller
+
+cd $dir
+java solve `xrandr --verbose | awk '/ connected /{}/Brightness:/{print $2}'` `xrandr | grep -w connected | awk -F'[ ]' '{print $1}'` 
+
+echo Copy this run.sh file to your home directory for easier use :)
